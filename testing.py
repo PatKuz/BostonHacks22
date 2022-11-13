027504b01089e1cb89bae9435fb151b1566bec01
Justin Sayah <55928969+justin-sayah@users.noreply.github.com>
2022-11-12 21:54:04 -0500

    Delete testing.py

--- testing.py
+++
@@ -1,7 +0,0 @@
-
-number = 7817388373
-uname = "justin"
-school = "bu"
-econ1 = "mom"
-econ2 = "dad"
-print('INSERT INTO Users (number, name, school, econ1, econ2) VALUES (\'{}\', \'{}\', \'{}\', \'{}\', \'{}\');'.format(number, uname, school, econ1, econ2))
